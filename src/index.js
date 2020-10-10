385dfed39eb32cc63db1ff072eb67f99a5456c77
XianB <xianbintang@outlook.com>
2020-10-10 14:51:38 +0800

    hander input and update template.yaml

--- src/index.js
+++ src/index.js
@@ -9,11 +9,47 @@ class CDNComponent extends Component {
   handlerInputs (inputs) {
     const properties = inputs.Properties || {}
     const credentials = inputs.Credentials || {}
+    const state = inputs.State || {}
+    const args = this.args(inputs.Args)
 
+    const CDNDomain = properties.CDNDomain || {}
+    const tags = properties.Tags || {}
+    const ipv6 = properties.Ipv6 || {}
+    const others = properties.Others || {}
+    const force = properties.Force || {}
+
+    const accessControl = properties.AccessControl || {}
+    const performance = properties.Performance || {}
+    const video = properties.Video || {}
+    const backToOrigin = properties.BackToOrigin || {}
+    const cache = properties.Cache || {}
+    const https = properties.Https || {}
+    // const DomainName = CDNDomain.DomainName ? CDNDomain.DomainName: {}
+    // const Source = CDNDomain.Source ? CDNDomain.Source: {}
+    // const Scope = CDNDomain.Scope ? CDNDomain.Scope: ''
+
+    return {
+      credentials,
+      state,
+      args,
+      CDNDomain,
+      tags,
+      ipv6,
+      others,
+      force,
+      accessControl,
+      performance,
+      video,
+      backToOrigin,
+      cache,
+      https
+    }
   }
   // 部署操作
+  // 1.1 首先添加域名
+  // 1.2 然后对域名进行配置
   async deploy(inputs) {
-    console.log('start deploy cdn config...')
+    console.log('cdn config deploying...')
     let client = await getCDNClient(inputs.Credentials)
     let params = {
       "scope":"domestic",
@@ -36,18 +72,37 @@ class CDNComponent extends Component {
   }
   // 移除操作
   async remove(inputs) {
-    console.log('start remove cdn config...')
+    console.log('cdn config removing...')
     console.log('remove cdn config succeed')
   }
   // 刷新操作
   async refresh(inputs) {
-    console.log('start refresh cdn config...')
+    console.log('cdn config refreshing')
     console.log('refresh cdn config succeed')
   }
   // 预热操作
   async preload(inputs) {
-    console.log('start preload cdn config')
+    console.log('cdn config preloading')
     console.log('preload cdn config succeed')
+  }
+  async stop(inputs) {
+    console.log('cdn config stopping')
+    console.log('stop cdn config succeed')
+  }
+  async start(inputs) {
+    console.log('cdn config starting')
+    console.log('start cdn config succeed')
+  }
+  async status(inputs) {
+    console.log('cdn config starting')
+    const {
+      credentials,
+      state,
+      args,
+      tags
+    } = this.handlerInputs(inputs)
+    console.log(tags)
+    console.log('start cdn config succeed')
   }
 }
 
